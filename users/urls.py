630a0289dce1b24f5f2cb54cb4a8b61b20289f4a
Ankit Malviya <99110050+Cruze08@users.noreply.github.com>
2024-01-11 21:56:29 +0530

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+"""Define URL patterns for users"""
+
+from django.contrib.auth.views import LoginView
+
+from django.urls import path
+from . import views
+app_name = "users"
+urlpatterns = [
+    # login page
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # Logout page
+    path('logout/', views.logout_view, name='logout'),
+
+    # Registration page
+    path('register/', views.register, name='register'),
+]
